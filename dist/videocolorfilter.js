a90dc9a84a4181abacd5c00c2fbce48799887275
Anwesh43 <anweshthecool0@gmail.com>
2017-04-29 17:26:23 +0530

    javascript dist file

---
+++ dist/videocolorfilter.js
@@ -0,0 +1,40 @@
+class ColorFilter {
+    constructor(src,color,img) {
+        this.img = img
+        this.video = document.createElement('video')
+        this.video.src = src
+        this.video.autoplay = true
+        this.video.height = window.innerHeight
+        this.video.width = window.innerWidth
+        this.color = color
+        this.canvas = document.createElement('canvas')
+        this.canvas.width = this.video.width
+        this.canvas.height = this.video.height
+        this.context = this.canvas.getContext('2d')
+    }
+    drawFilter() {
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
+        this.context.drawImage(this.video,0,0,this.canvas.width,this.canvas.height)
+        this.context.globalAlpha = 0.5
+        this.context.fillStyle = this.color
+        this.context.fillRect(0,0,this.canvas.width,this.canvas.height)
+    }
+    renderVideo() {
+        setInterval(()=>{
+          this.drawFilter()
+          const dataUrl = this.canvas.toDataURL()
+          this.img.src = dataUrl
+        },100)
+    }
+}
+
+class VideoColorFilter {
+    constructor(src,color) {
+        const img = document.createElement('img')
+        this.colorFilter = new ColorFilter(src,color,img)
+        document.body.appendChild(img)
+    }
+    createVideoFilter() {
+        this.colorFilter.renderVideo()
+    }
+}
