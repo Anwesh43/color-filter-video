ae1a09c07dc99f9d192f61fe9d41e7502f6ef764
Anwesh43 <anweshthecool0@gmail.com>
2017-04-29 18:23:41 +0530

    new dist file and dependencies

--- dist/videocolorfilter.js
+++ dist/videocolorfilter.js
@@ -11,6 +11,7 @@ class ColorFilter {
         this.canvas.width = this.video.width
         this.canvas.height = this.video.height
         this.context = this.canvas.getContext('2d')
+        this.video.loop = true
     }
     drawFilter() {
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
