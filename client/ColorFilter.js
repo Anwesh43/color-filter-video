6b6cdbe64fa332e3f8db5757101e060a4bd4d87d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-29 16:17:02 +0530

    Adding renderVideo method

--- client/ColorFilter.js
+++ client/ColorFilter.js
@@ -1,7 +1,9 @@
-class ColorFilter {
-    constructor(src,color) {
+class VideoColorFilter {
+    constructor(src,color,img) {
+        this.img = img
         this.video = document.createElement('video')
         this.video.src = src
+        this.video.autoplay = true
         this.video.width = window.innerHeight
         this.video.height = window.innerWidth
         this.color = color
@@ -11,9 +13,17 @@ class ColorFilter {
         this.context = this.canvas.getContext('2d')
     }
     drawFilter() {
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
         this.conext.drawImage(this.video,0,0,this.canvas.width,this.canvas.height)
         this.context.globalAlpha = 1
         this.context.fillStyle = color
         this.context.fillRect(0,0,canvas.width,canvas.height)
+    }
+    renderVideo() {
+        setInterval(()=>{
+          this.drawFilter()
+          const dataUrl = this.canvas.toDataURL()
+          this.img.src = dataUrl
+        },100)
     }
 }
